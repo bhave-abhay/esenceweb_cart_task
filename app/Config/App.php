e9d211b98509405601654a747f313ed2ef0ed68d
Abhay Bhave <bhave.abhay@gmail.com>
2022-08-13 21:19:25 +0530

    Corrected basepath and index path

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/esenceweb_cart_task/public/';
+    public $baseURL = 'http://localhost/esenceweb_cart_task/';
 
     /**
      * --------------------------------------------------------------------------
@@ -37,7 +37,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $indexPage = 'index.php';
+    public $indexPage = '';
 
     /**
      * --------------------------------------------------------------------------
