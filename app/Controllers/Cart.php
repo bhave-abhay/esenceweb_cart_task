f5383045619a356c3d54f57b8784d5a7a0777ae5
Abhay Bhave <bhave.abhay@gmail.com>
2022-08-14 23:58:13 +0530

    Hosted JQuery, Bootstrap and DataTables files locally instead of CDN

--- app/Controllers/Cart.php
+++ app/Controllers/Cart.php
@@ -33,15 +33,15 @@ class Cart extends BaseController
                 'arrFootData' => [
                     'arrScript' => [
                         [
-                            'src'=>"https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js",
+                            'src'=>"Resources/bootstrap-5.2.0-dist/js/bootstrap.bundle.min.js",
                             'integrity'=>"sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa",
                             'crossorigin'=>"anonymous"
+                        ],
+                        [
+                            'src' => "Resources/jquery-3.6.0-dist/jquery-3.6.0.min.js",
+                            'integrity' => "sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=",
+                            'crossorigin' => "anonymous"
                         ]
-                    ],
-                    [
-                        'src' => "https://code.jquery.com/jquery-3.6.0.min.js",
-                        'integrity' => "sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=",
-                        'crossorigin' => "anonymous"
                     ]
                 ]
             ]
--- app/Views/html_head.php
+++ app/Views/html_head.php
@@ -8,9 +8,12 @@
     <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
     <link
         rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+        href="Resources/bootstrap-5.2.0-dist/css/bootstrap.min.css"
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
         crossorigin="anonymous" />
+        <link
+            rel="stylesheet"
+            href="Resources/DataTables/datatables.min.css"/>
 </head>
 <body>
     <div class="container-fluid"><!-- START OF MAIN CONTAINER -->
